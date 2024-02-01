21dbaa16d7cd4b8a25f6287166eae96c23744baa
Dilyorbek <justsharofiddinov@gmail.com>
2024-02-01 21:30:12 +0500

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="flex h-screen w-screen bg-[#163c3d] p-5 justify-center items-center">
+      <h1 className="text-white text-4xl md:text-6xl lg:text-9xl text-center font-semibold font-mono leading-10 md:leading-12 lg:leading-2">
+        Something fresh and captivating is on its way!
+      </h1>
+    </div>
+
+  );
+}
